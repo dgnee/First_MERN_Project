6b748a05fe4b4ee0e73cfb7482e71f475a2956f5
Sojin Lee <0o0vghfjdk@naver.com>
2022-11-12 14:57:52 +0900

    Added mongodb connection

--- server/server.js
+++ server/server.js
@@ -1,13 +1,30 @@
-const express = require('express') // to setup a backend, 'express' needs to be imported
+const express = require("express") // to setup a backend, 'express' needs to be imported
+const mongoose = require("mongoose");
+require("dotenv").config();
+const cors = require("cors");
 const app = express() // to create an app
 
+
+// connect to mongoose
+mongoose
+    .connect(process.env.MONGO_URL, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    })
+    .then(() => console.log("Mongodb Connected..."))
+    .catch((err) => console.error(err));
+
+/* Routing is not used for the MERN stack todo app exercise
 // set up a route for the api
 app.get("/api",(req, res) => {
     res.json({"users": ["userOne", "userTwo", "userThree", "userFour"]})// send json array
     // {"users": ["userOne", "userTwo", "userThree"]} is basically our backend api
 })
+*/
 
 // start up the backeend
-app.listen(3000, () => {console.log("Server started on port 3000")})
+app.listen(5000, () => {
+    console.log("Server started on port 5000");
+});
 
 // start up the server 
